95da70594e481127601f3fb074623894b2d7633b
Ovubs <apple@APPLEs-MacBook-Pro.local>
2024-01-03 12:51:57 +0100

    added root route to backend

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -3,6 +3,12 @@ const express = require("express");
 const route = express.Router();
 const { sendEmail } = require("../controllers/getProjects");
 
+route.get('/', async (req,res) => {
+ 
+  res.status(200).json({
+    success:'true'
+  })
+})
 route.post('/api/send-email', sendEmail)
 
 module.exports = route
